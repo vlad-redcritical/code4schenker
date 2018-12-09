8b25cec8459cc54ccebf9d7f2c18b6fa00d9c567
Damian Dabrowski <dabek1812@gmail.com>
2018-12-09 10:38:48 +0100

    Logo

--- dashboard/src/views/Home.vue
+++ dashboard/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home d-flex justify-content-center">
-    <img alt="Vue logo" src="../assets/logo.jpg">
+    <img class="mt-5" style="max-width: 80%; height: 220px;" alt="Vue logo" src="../assets/logo.jpg">
   </div>
 </template>
 
